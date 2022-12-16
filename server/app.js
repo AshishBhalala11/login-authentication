fa6d0067a586ca37f82b0c3b463a2e2d67df68b6
Ashish-SmartHubAI <ashish.bhalala@smarthub.ai>
2022-12-17 00:11:41 +0530

    changes to server side

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const server = jsonServer.create();
 const userdb = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
 
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(jsonServer.defaults());
 
@@ -15,29 +15,29 @@ const ACCESS_TOKEN_KEY = "963258741";
 const expiresIn = "1h";
 
 function getAccessToken(payload) {
-    return jwt.sign(payload, ACCESS_TOKEN_KEY, {expiresIn});
+    return jwt.sign(payload, ACCESS_TOKEN_KEY, { expiresIn });
 }
 
-function isLoginAuthenticated({email, password}) {
+function isLoginAuthenticated({ email, password }) {
     return (
         userdb.users.findIndex(
             (user) => user.email === email && user.password === password) !== -1
     );
 }
 
-server.post("/api/auth/login", (req,res) => {
-    const {email, password} = req.body;
-    if (!isLoginAuthenticated({email, password})) {
+server.post("/api/auth/login", (req, res) => {
+    const { email, password } = req.body;
+    if (!isLoginAuthenticated({ email, password })) {
         const status = 401;
         const message = "Incorrect Email or Password";
         res.status(status).json({ status, message });
         return;
     }
-    const access_token = getAccessToken({email, password});
-    res.status(200).json({access_token});
+    const access_token = getAccessToken({ email, password });
+    res.status(200).json({ access_token });
 });
 
-server.get("/api/auth/token", (req,res) => {
+server.get("/api/auth/token", (req, res) => {
     const header = req.headers['authorization'];
     if (typeof header !== 'undefined') {
         const headers = header.split(' ');
